f401ec515b1b99b96a83b76296240d99f792689f
nidhijain128 <nidhiprj@usc.edu>
2014-09-12 02:20:56 -0700

    default.php

---
+++ nidhijain.php
@@ -0,0 +1,178 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Welcome to Nidhi's World!! :D</title>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/pure-min.css">
+	<link rel="stylesheet" href="metro-bootstrap.css">
+	<link rel="stylesheet" href="style.css">
+	<!--<link rel="stylesheet" href="semantic.css">-->
+	<link rel="stylesheet" href="semantic.min.css">
+	
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="true">
+	
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50627221-1', 'usc.edu');
+  ga('send', 'pageview');
+</script>
+	<script src="allscripts.js"></script>
+	<script src="metro.min.js"></script>
+	<script src="semantic.min.js"></script>
+	<script src="semantic.js"></script>
+	<link rel="icon" type="image/png" href="nidhi.jpg" />
+
+</head>
+
+<body class="metro">
+	<div id="mainstrip">
+		<nav class="navigation-bar dark bg-gray">
+			<nav class="navigation-bar-content">
+				<span class="header header1">Nidhi Praveen Jain</span>
+				<a href="https://www.facebook.com/nidhijain1208" target="_blank" class="proflinks"><img src="facebook.png" data-src-240px="image-600px.jpg"
+				data-src-320px="image-800px.jpg" title="Facebook" height="40px" width="40px"></a>
+				<a href="http://www.linkedin.com/pub/nidhi-jain/25/855/25b/" target="_blank" class="proflinks"><img title="LinkedIn" src="linkedin.png" height="44px" width="44px" align="right"></a>
+				<a href="https://github.com/nidhijain128" target="_blank" class="proflinks"><img src="github.jpg" title="GitHub" height="40px" width="40px" align="right"></a>
+				<a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=nidhijain1208@gmail.com&body=" target="_blank" class="proflinks"><img title="Gmail" src="gmail.png" height="42px" width="42px" align="right"></a>
+			</nav>
+		</nav>
+		<div id="main">
+			<div class="ui tabular menu">
+				<a class="active item" href="#" id="button1" onclick="nidhiclicked()">Nidhi</a>
+				<a class="item" href="#" id="button2" onclick="profileclicked()">Profile</a>
+				<a class="item" href="#" id="button3" onclick="projectsclicked()">Projects</a>
+				<a class="item" href="#" id="button4" onclick="coursesclicked()">Courses</a>
+			</div>
+			<div class="tabdata">
+				<div id="nidhi">
+					<p class="nidhipicdata">
+						<img src="nidhi.jpg" height="35%" width="15%" id="nidhipic">
+						<p>I am a graduate student at University of Southern California (USC) pursuing my Masters in Computer Science. I wish to learn a lot in the field of Web and mobile application development and make a career in the same field.</p>
+						<p>I love meeting and talking to people. I believe there is something to learn from each one, and every person is unique. </p>
+					</p>
+				</div>
+				<div id="profile">
+					<ul>
+						<li>Education
+						<ol>
+							<li>University of Southern California(USC), Los Angeles - Joined as a graduate student in Fall 2013. I expect to graduate by Spring 2015. At USC, I am doing my Masters in Computer Science(General) but I am taking courses based on my interest towards web application development and databases.
+							I am also working on website development of student group <a target="_blank" href="http://www.aisusc.org">AIS</a> (Association of Indian Students)
+							<li>Vidyalankar Institute of Technology(VIT), Mumbai, India - I completed my Bachelors of Engineering in Computer Engineering from VIT. In the course of 4 years, I have done various small and big projects. I was the moderator and site representative of the <a target="_blank" href="http://www.vidyalankarlive.com">college portal</a>.
+							I was actively involved in extra-curricular activities as well. I have led the college in various inter-college sports events like Football, Throwball and Dodgeball. Being the sports captain, I have organized and conducted various events like Cricket, Amazing Race, Football etc.</li>
+						</ol>
+						</li>
+						<li>Work Experience
+						<ol>
+							<li>MathWorks Inc (Summer Internship) - I am working as an Application Support Engineer providing support/solution to clients. I was also involved in the improvement of an internal tool.</li>
+							<li>USC Graduate Schoool (On-Campus) - I was working as the Graduate Database Assistant. It involves writing SQL queries for various types of surveys reports and for the Graduate School Applications.</li>
+							<li>USC School of Social Work (On-campus) - I worked in the research team of the project "Welcoming Practices" as a web developer. The project mainly focusses to ease the transition of military students from one school to another. I am working to create a web and mobile application in order to make this transition electronic. Also, maintaining their research project website <a target="_blank" href="http://buildingcapacity.usc.edu/">"Building Capacity."</a></li>
+							<li>Infosys Technologies Limited - I have worked at Infosys for 1 year and 7 months. Initially, I was posted as a Systems Engineer Trainee and was later promoted to Systems Engineer. I worked in the Execution Edge team of Business Platform(BizP) unit. We mainly dealt with a project management tool, Clarity PPM and all my projects were based on this tool.</li>
+						</ol>
+						</li>
+				</div>
+				<div id="projects">
+					<ul>
+						<li>USC
+							<ol>
+								<li>Information Retrieval and Web Search Engines - Working on extracting data from website, crawling the world wide web using Apache Tika, Nutch and Solr.</li>
+								<li>Advanced Data Stores - Working on benchmarking various data stores like HBase, Cassandra, VoltDB, Voldemort using <a target="_blank" href="http://www.bgbenchmark.org">BG tool</a></li>
+								<li>Android Mobile Application - Created an application that displays weather forecast for the next 5 days. It was hosted using Amazon Web Services</li>
+								<li>Public Announcement System - Created an application that queries the spatial database. It keeps track of all the areas that various announcement systems cover. </li>
+							</ol>
+						</li>
+						<li>Infosys
+							<ol>
+								<li>GSK - Our team along with another team at Infosys worked on creating an application for GSK. We used Clarity PPM as a backend to this app. I was involved in writing processes and gel scripts for various events that occured.</li>
+								<li>Amex - I was working on the migration of Clarity from V12 to V13. I was involved in writing test cases and perform theses tests on the various testing and production environments.</li>
+								<li>Mobility - We created a mobile version of the tool that displayed important data like upcoming tasks, dashboards etc. I was done by making eb service calls to the tool in order to read and write data into the Database.</li>
+							</ol>
+						</li>
+						<li>VIT
+							<ol>
+								<li>Final year project - I along with 2 team members, created a 2D game using Microsofts XNA Framework.
+									<ul>
+										<li><a target="_blank" href="RajniPhysicsIEEEPaper.pdf">Technical Paper</a></li>
+										<li><a href="RajniPhysicsBlackBook.pdf" target="_blank">Documentation</a></li>
+										<li><a href="RajniPhysicsGameplay.3gp" target="_blank">Gameplay</a></li>
+									</ul>
+								</li>
+							</ol>
+						</li>
+					<ul>
+				</div>
+				<div id="courses">
+					<ul>
+						<li>Current Courses at USC
+							<ol>
+								<li>Artificial Intelligence</li>
+								<li><a target="_blank" href="http://web-app.usc.edu/soc/syllabus/20141/30000.pdf">Advanced Data Stores</a></li>
+								<li><a target="_blank" href="http://www-scf.usc.edu/~csci572/">Information Retrieval and Web Search Engines</a></li>
+							</ol></li>
+						<li>Previous courses at USC
+							<ol>
+								<li><a target="_blank" href="http://www-scf.usc.edu/~csci571/">Web Technologies</a> <!--- <a href="" onclick="webtechclicked()">List of Homeworks</a>--></li>
+								<li><a target="_blank" href="http://www-scf.usc.edu/~csci585/assigments.html">Database Systems</a></li>
+								<li><a target="_blank" href="http://merlot.usc.edu/cs570-f13/">Analysis of Algorithms</a></li>
+							</ol>
+						</li>
+					</ul><br>
+				</div>
+				<div id="wtdata">
+					<h1> List of Homeworks - Nidhi Praveen Jain</h1><br /><br /><br />
+					<table border="1" row="5" column="2" bgcolor="#B6B5B9" cellpadding="5" cellspacing="2">
+											<tr>
+												<td><a href="HW3.html">Link to Homework #3</a></td>
+												<td><a href="HW4.html">Link to Homework #4</a></td>
+											</tr>
+											<tr>
+												<td><a href="HW5.html">Link to Homework #5</a></td>
+												<td><a href="HW6.html">Link to Homework #6</a></td>
+											</tr>
+											<tr>
+												<td><a href="http://default-environment-a6bt8376s9.elasticbeanstalk.com/?location=90089&locationType=zip&tempUnit=f">Link to Homework #7</a></td>
+												<td><a href="http://cs-server.usc.edu:13856/HomeWork8/HW8.html">Link to Homework #8</a></td>
+											</tr>
+										</table>
+									</div>
+				<!--<div id="quotes">
+					I like to phrase my thoughts at times. 
+				</div>
+				<div id="contactme">
+					
+				</div>-->
+			</div>
+		</div>
+		<div class="separationline"></div>
+		<nav class="navigation-bar dark bg-gray footerPosition">
+			<nav class="navigation-bar-content">
+				<p>Copyright &copy; Nidhi Praveen Jain - nidhiprj@usc.edu</p>
+			</nav>
+		</nav>
+	</div>
+
+<?php
+	$url = "http://where.yahooapis.com/v1/concordance/usps/90007?appid=5M3Huy_V34HFgLHJTXK42jTt4ZZiwOVLU5R6wwKmckcyiYV1qdpdWtI2c6V6VOT5t7wyKA--";
+	echo $url;
+	$uwoeidN = file_get_contents($url);
+	$pwoeidN = simplexml_load_string($url);
+	echo $pwoeidN;
+	if($uwoeidN != "")
+	{
+		//$woeidValue = substr($uwoeidN,0,-5);
+
+		echo strlen($uwoeidN);
+		$urlW = "";
+		$urlW = "http://weather.yahooapis.com/forecastrss?w=".$woeidValue."&u=c";
+		$uweather = file_get_contents($urlW);
+		//$pweather = simplexml_load_string($uweather);
+		echo $uweather;
+	}
+?>
+<noscript>
+<!--</body>-->
+</html>
