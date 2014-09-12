0578c24b57c48dccbc4a9b3916877752563d96cf
nidhijain128 <nidhiprj@usc.edu>
2014-09-12 02:21:01 -0700

    default.php

---
+++ default.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nidhi Jain</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+	<link href="css/jquery-ui.css" rel="stylesheet" type="text/css">
+    <link href="css/demo.css" rel="stylesheet" type="text/css">
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+	
+	<script type="text/javascript" src="js/jquery-1.7.1.js"></script>
+	<script type="text/javascript" src="js/jquery-ui-1.8.20.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/bootstrap-alert.js"></script>
+	<script type="text/javascript" src="js/bootstrap-tab.js"></script>
+    <script src="js/scrolld.min.js" type="text/javascript"></script>
+    <script src="js/allscripts.js" type="text/javascript"></script>
+    
+    <meta content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" name="viewport">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta content="true" name="HandheldFriendly">
+    <script type="text/javascript">
+		//<![CDATA[
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                ga('create', 'UA-50627221-1', 'usc.edu');
+                ga('send', 'pageview');
+		//]]>
+    </script>
+    <link href="images/nidhi.jpg" rel="icon" type="image/png">
+</head>
+
+<body class="metro">
+    <nav class="navigation-bar colorBar">
+        <nav class="navigation-bar-content">
+            <div id="fixedNavBar" class="tabbable">
+                <ul id="" class="nav nav-tabs">
+                    <li class="active buttonWidth"><a data-toggle="tab" href="#nidhi" id="">Home</a></li>
+                    <li class="buttonWidth"><a data-toggle="tab" href="#academia" id="">Academia</a></li>
+                    <li class="buttonWidth"><a data-toggle="tab" href="#professional" id="">Professional</a></li>
+                    <li class="buttonWidth"><a data-toggle="tab"  href="#projects" id="">Projects</a></li>
+                </ul>
+			</div>
+		</nav>
+	</nav>
+				<div class="tab-content" id="fullSectionWrap">
+				<div class="tab-pane active fullSection" id="nidhi">
+					<div class="sectionContent">
+						<div class="title">Nidhi Praveen Jain</div><br><br>
+						<p class="mainContent1">Computer Science Graduate Student, University of Southern California <br>
+						</p>
+					</div>
+				</div>
+				<div class="tab-pane fullSection" id="academia">
+					<div class="sectionContent" style="height: 100%">
+						<h1>Academia</h1><br><br>
+						<div class="accordion" id="accordion1">
+							<div class="accordion-group">
+								<div class="accordion-heading">
+									<a class="accordion-toggle fontStyle" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne">
+										University of Southern California
+									</a>
+								</div>
+								<div id="collapseOne" class="accordion-body collapse">
+									<div class="accordion-inner textAlign">
+										Fall 2013:<br>
+										<a target="_blank" href="http://merlot.usc.edu/cs570-f13/">CSCI 570 - Analysis of Algorithms</a>
+										<a target="_blank" href="http://www-scf.usc.edu/~csci571/">CSCI 571 - Web Technologies</a>
+										<a target="_blank" href="http://www-scf.usc.edu/~csci585/assigments.html">CSCI 585 - Database Systems</a><br>
+										Spring 2014:<br>
+										<a target="_blank" href="http://web-app.usc.edu/soc/syllabus/20141/30000.pdf">CSCI 550 - Advanced Data Stores</a>
+										CSCI 561 - Artificial Intelligence
+										<a target="_blank" href="http://www-scf.usc.edu/~csci572/">CSCI 572- Information Retrieval and Web Search Engines</a>
+									</div>
+								</div>
+							</div>
+							<div class="accordion-group">
+								<div class="accordion-heading">
+									<a class="accordion-toggle fontStyle" data-toggle="collapse" data-parent="#accordion1" href="#collapseTwo">
+										Vidyalankar Instirute of Technology
+									</a>
+								</div>
+								<div id="collapseTwo" class="accordion-body collapse">
+									<div class="accordion-inner">
+										Lorem Ipsum
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fullSection" id="professional">
+					<div class="sectionContent">
+						<h1>Professional</h1><br><br>
+						<div class="accordion" id="accordion2">
+							<div class="accordion-group">
+								<div class="accordion-heading">
+									<a class="accordion-toggle fontStyle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+										Application Support Engineer, MathWorks
+									</a>
+								</div>
+								<div id="collapseOne" class="accordion-body collapse">
+									<div class="accordion-inner textAlign">
+						
+									</div>
+								</div>
+							</div>
+							<div class="accordion-group">
+								<div class="accordion-heading">
+									<a class="accordion-toggle fontStyle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+										Web Developer and Database Assistant, USC School of Social Work
+									</a>
+								</div>
+								<div id="collapseTwo" class="accordion-body collapse">
+									<div class="accordion-inner">
+										
+									</div>
+								</div>
+							</div>
+							<div class="accordion-group">
+								<div class="accordion-heading">
+									<a class="accordion-toggle fontStyle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+										Database Assitant, USC Graduate School
+									</a>
+								</div>
+								<div id="collapseThree" class="accordion-body collapse">
+									<div class="accordion-inner">
+										
+									</div>
+								</div>
+							</div>
+							<div class="accordion-group">
+								<div class="accordion-heading">
+									<a class="accordion-toggle fontStyle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
+										Systems Engineer, Infosys Technologies Limited
+									</a>
+								</div>
+								<div id="collapseFour" class="accordion-body collapse">
+									<div class="accordion-inner">
+										
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fullSection" id="projects">
+					<div class="sectionContent">
+						<h1>Projects</h1>
+						<p class="content"></p>
+					</div>
+				</div>
+						</div>
+					
+					
+					<!--<div id="headerRight">
+                <img src="" id="weatherImg"/><br>
+                <span id="weatherDetails"></span>
+            <div>-->
+        </nav>
+    </nav>
+
+    <!--div id="fullSectionWrap">
+        <div class="fullSection" id="nidhi">
+            <div class="sectionContent">
+                <h1>Nidhi Praveen Jain</h1><br>
+                <p class="content">Computer Science Graduate Student, University of Southern California 
+					<span style="color: #FF003D;"></span><br>
+				</p>
+            </div>
+        </div>
+
+        <div class="fullSection" id="academia">
+            <div class="sectionContent">
+                <h1>Academia</h1>
+                <p class="">
+					<div id="accordion">
+						<h1>University of Southern California</h1>
+						<div>
+							Fall 2013:
+							<ol>
+								<li><a target="_blank" href="http://merlot.usc.edu/cs570-f13/">CSCI 570 - Analysis of Algorithms</a></li>
+								<li><a target="_blank" href="http://www-scf.usc.edu/~csci571/">CSCI 571 - Web Technologies</a> </li>
+                                <li><a target="_blank" href="http://www-scf.usc.edu/~csci585/assigments.html">CSCI 585 - Database Systems</a></li>
+							</ol>
+							Spring 2014:
+							<ol>
+								<li><a target="_blank" href="http://web-app.usc.edu/soc/syllabus/20141/30000.pdf">CSCI 550 - Advanced Data Stores</a></li>
+								<li>CSCI 561 - Artificial Intelligence</li>
+                                <li><a target="_blank" href="http://www-scf.usc.edu/~csci572/">CSCI 572- Information Retrieval and Web Search Engines</a></li>
+							</ol>
+						</div>
+						<h1>Vidyalankar Institute of Technology</h1>
+						<div >
+							Southern California
+						</div>
+					</div>
+				</p>
+                <!--<ol>
+                    <li>University of Southern California(USC), Los Angeles - Joined as a graduate student in Fall 2013. I expect to graduate by Spring 2015. At USC, I am doing my Masters in Computer Science(General) but I am taking courses based on my interest towards web application development and databases. I am also working on website development of student group
+                    <a href="http://www.aisusc.org" target="_blank">AIS</a> (Association of Indian Students)
+                    </li>
+					
+                    <li>Vidyalankar Institute of Technology(VIT), Mumbai, India - I completed my Bachelors of Engineering in Computer Engineering from VIT. In the course of 4 years, I have done various small and big projects. I was the moderator and site representative of the 
+					<a href="http://www.vidyalankarlive.com" target="_blank">college portal</a>. I was actively involved in extra-curricular activities as well. I have led the college in various inter-college sports events like Football, Throwball and Dodgeball. Being the sports captain, I have organized and conducted various events like Cricket, Amazing Race, Football etc.
+                    </li>
+                </ol><span style="color: #FF003D;"></span><br>>
+                <br>
+            </div>
+        </div>
+
+        <div class="fullSection" id="professional">
+            <div class="sectionContent">
+                <h1>Professional</h1>
+                <p class="content"></p>
+                <ol>
+                    <li>MathWorks Inc (Summer Internship) - I am working as an Application Support Engineer providing support/solution to clients. I was also involved in the improvement of an internal tool.</li>
+
+                    <li>USC Graduate Schoool (On-Campus) - I was working as the Graduate Database Assistant. It involves writing SQL queries for various types of surveys reports and for the Graduate School Applications.</li>
+
+                    <li>USC School of Social Work (On-campus) - I worked in the research team of the project "Welcoming Practices" as a web developer. The project mainly focusses to ease the transition of military students from one school to another. I am working to create a web and mobile application in order to make this transition electronic. Also, maintaining
+                    their research project website <a href="http://buildingcapacity.usc.edu/" target="_blank">"Building Capacity."</a>
+                    </li>
+
+                    <li>Infosys Technologies Limited - I have worked at Infosys for 1 year and 7 months. Initially, I was posted as a Systems Engineer Trainee and was later promoted to Systems Engineer. I worked in the Execution Edge team of Business Platform(BizP) unit. We mainly dealt with a project management tool, Clarity PPM and all my projects were based on this
+                    tool.</li>
+                </ol><br>
+                <span style="color: #FF003D;"></span><br><br><br>
+            </div>
+        </div>
+
+        <div class="fullSection" id="coolStuff">
+            <div class="sectionContent">
+                <h1>Projects</h1>
+                <p class="content"></p>
+                <ul>
+                    <li>USC
+                        <ol>
+                            <li>Information Retrieval and Web Search Engines - Working on extracting data from website, crawling the world wide web using Apache Tika, Nutch and Solr.</li>
+
+                            <li>Advanced Data Stores - Working on benchmarking various data stores like HBase, Cassandra, VoltDB, Voldemort using <a href="http://www.bgbenchmark.org" target="_blank">BG tool</a>
+                            </li>
+
+                            <li>Android Mobile Application - Created an application that displays weather forecast for the next 5 days. It was hosted using Amazon Web Services</li>
+
+                            <li>Public Announcement System - Created an application that queries the spatial database. It keeps track of all the areas that various announcement systems cover.</li>
+                        </ol>
+                    </li>
+                    <li>Infosys
+                        <ol>
+                            <li>GSK - Our team along with another team at Infosys worked on creating an application for GSK. We used Clarity PPM as a backend to this app. I was involved in writing processes and gel scripts for various events that occured.</li>
+
+                            <li>Amex - I was working on the migration of Clarity from V12 to V13. I was involved in writing test cases and perform theses tests on the various testing and production environments.</li>
+
+                            <li>Mobility - We created a mobile version of the tool that displayed important data like upcoming tasks, dashboards etc. I was done by making eb service calls to the tool in order to read and write data into the Database.</li>
+                        </ol>
+                    </li>
+
+                    <li>VIT
+                        <ol>
+                            <li>Final year project - I along with 2 team members, created a 2D game using Microsofts XNA Framework.
+                                <ul>
+                                    <li><a href="attachments/RajniPhysicsIEEEPaper.pdf" target="_blank">Technical Paper</a></li>
+                                    <li><a href="attachments/RajniPhysicsBlackBook.pdf" target="_blank">Documentation</a></li>
+                                    <li><a href="attachments/RajniPhysicsGameplay.3gp" target="_blank">Gameplay</a></li>
+                                </ul>
+                            </li>
+                        </ol>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div-->
+	<div id="footerBar">
+                <div id="footerLeft"><p>Copyright &copy; Nidhi Praveen Jain - nidhiprj@usc.edu</p></div>
+                <div id="footerRight">    
+					<a class="proflinks" href="https://www.facebook.com/nidhijain1208" target="_blank">
+						<img data-src-240px="image-600px.jpg" data-src-320px="image-800px.jpg" height="40px" src= "images/facebook.png" title="Facebook" width="40px">
+					</a> 
+					<a class="proflinks" href="http://www.linkedin.com/pub/nidhi-jain/25/855/25b/" target="_blank">
+						<img align="right" height="44px" src="images/linkedin.png" title="LinkedIn" width="44px">
+					</a> 
+					<a class="proflinks" href="https://github.com/nidhijain128" target="_blank">
+						<img align="right" height="40px" src="images/github.jpg" title="GitHub" width="40px">
+					</a> 
+					<a class="proflinks" href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=nidhijain1208@gmail.com&body=" target="_blank">
+						<img align="right" height="42px" src="images/gmail.png" title="Gmail" width="42px">
+					</a></div>
+            </div>
+            <!--<div class="tab-content" id="myTabContent">
+                <div id="nidhi" class="tab-pane fade in active">
+                    <p class="nidhipicdata">
+                        <img src="nidhi.jpg" height="40%" width="15%" id="nidhipic">
+                        <p>I am a graduate student at University of Southern California (USC) pursuing my Masters in Computer Science. I wish to learn a lot in the field of Web and mobile application development and make a career in the same field.</p>
+                        <p>I love meeting and talking to people. I believe there is something to learn from each one, and every person is unique. </p>
+                    </p>
+                </div>
+                
+                <div id="courses" class="tab-pane fade">
+                    <ul>
+                        <li>Current Courses at USC
+                            <ol>
+                                <li>Artificial Intelligence</li>
+                                <li><a target="_blank" href="http://web-app.usc.edu/soc/syllabus/20141/30000.pdf">Advanced Data Stores</a></li>
+                                <li><a target="_blank" href="http://www-scf.usc.edu/~csci572/">Information Retrieval and Web Search Engines</a></li>
+                            </ol></li>
+                        <li>Previous courses at USC
+                            <ol>
+                                <li><a target="_blank" href="http://www-scf.usc.edu/~csci571/">Web Technologies</a> <!=== <a href="" onclick="webtechclicked()">List of Homeworks</a></li>
+                                <li><a target="_blank" href="http://www-scf.usc.edu/~csci585/assigments.html">Database Systems</a></li>
+                                <li><a target="_blank" href="http://merlot.usc.edu/cs570-f13/">Analysis of Algorithms</a></li>
+                            </ol>
+                        </li>
+                    </ul><br>
+                </div>
+            </div>
+        </div>-->
+    <!--<div class="separationline"></div>
+        <nav class="navigation-bar colorBar footerPosition">
+            <nav class="navigation-bar-content">
+                <div id="footerLeft"><p>Copyright &copy; Nidhi Praveen Jain - nidhiprj@usc.edu</p></div>
+                <div id="footerRight"><a href="https://www.facebook.com/nidhijain1208" target="_blank" class="proflinks"><img src="facebook.png" data-src-240px="image-600px.jpg"
+                data-src-320px="image-800px.jpg" title="Facebook" height="40px" width="40px"></a>
+                <a href="http://www.linkedin.com/pub/nidhi-jain/25/855/25b/" target="_blank" class="proflinks"><img title="LinkedIn" src="linkedin.png" height="44px" width="44px" align="right"></a>
+                <a href="https://github.com/nidhijain128" target="_blank" class="proflinks"><img src="github.jpg" title="GitHub" height="40px" width="40px" align="right"></a>
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=nidhijain1208@gmail.com&body=" target="_blank" class="proflinks"><img title="Gmail" src="gmail.png" height="42px" width="42px" align="right"></a></div>
+            </nav>
+        </nav>-->
+    <!--</div>-->
+    
+    <script type="text/javascript">
+		$(".collapse").collapse();
+		$("[id*='Btn']").stop(true).on('click',function(e){e.preventDefault();$(this).scrolld();});
+    </script> 
+<noscript>
+<!--</body>-->
+</html>
